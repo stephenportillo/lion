e494222c18620bff3426d69459f8c8bd7eb73bef
tdaylan <tansu.daylan@gmail.com>
2018-06-14 10:36:05 -0400

    Changed C interface for temporal modeling

--- image_eval.py
+++ image_eval.py
@@ -1,45 +1,66 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from __init__ import *
 
-def psf_poly_fit(psf0, nbin):
-        assert psf0.shape[0] == psf0.shape[1] # assert PSF is square
-        npix = psf0.shape[0]
+def psf_poly_fit(psfnusam, factusam):
+    
+    assert psfnusam.shape[0] == psfnusam.shape[1] # assert PSF is square
+    
+    # number of pixels along the side of the upsampled PSF
+    numbpixlpsfnsideusam = psfnusam.shape[0]
 
-        # pad by one row and one column
-        psf = np.zeros((npix+1, npix+1), dtype=np.float32)
-        psf[0:npix, 0:npix] = psf0
+    # pad by one row and one column
+    psfnusampadd = np.zeros((numbpixlpsfnsideusam+1, numbpixlpsfnsideusam+1), dtype=np.float32)
+    psfnusampadd[0:numbpixlpsfnsideusam, 0:numbpixlpsfnsideusam] = psfnusam
 
-        # make design matrix for each nbin x nbin region
-        nc = npix/nbin # dimension of original psf
-        nx = nbin+1
-        y, x = np.mgrid[0:nx, 0:nx] / np.float32(nbin)
-        x = x.flatten()
-        y = y.flatten()
-        A = np.column_stack([np.full(nx*nx, 1, dtype=np.float32), x, y, x*x, x*y, y*y, x*x*x, x*x*y, x*y*y, y*y*y]).astype(np.float32)
-        # output array of coefficients
-        cf = np.zeros((A.shape[1], nc, nc), dtype=np.float32)
+    # make design matrix for each factusam x factusam region
+    numbpixlpsfnside = numbpixlpsfnsideusam / factusam # dimension of original psf
+    nx = factusam + 1
+    y, x = np.mgrid[0:nx, 0:nx] / np.float32(factusam)
+    x = x.flatten()
+    y = y.flatten()
+    A = np.column_stack([np.full(nx*nx, 1, dtype=np.float32), x, y, x*x, x*y, y*y, x*x*x, x*x*y, x*y*y, y*y*y]).astype(np.float32)
+    
+    # number of subpixel parameters
+    numbparaspix = A.shape[1]
 
-        # loop over original psf pixels and get fit coefficients
-        for iy in xrange(nc):
-         for ix in xrange(nc):
-                # solve p = A cf for cf
-                p = psf[iy*nbin:(iy+1)*nbin+1, ix*nbin:(ix+1)*nbin+1].flatten()
-                AtAinv = np.linalg.inv(np.dot(A.T, A))
-                ans = np.dot(AtAinv, np.dot(A.T, p))
-                cf[:,iy,ix] = ans
+    # output array of coefficients
+    coefspix = np.zeros((numbparaspix, numbpixlpsfnside, numbpixlpsfnside), dtype=np.float32)
 
-        return cf.reshape(cf.shape[0], cf.shape[1]*cf.shape[2])
+    # loop over original psf pixels and get fit coefficients
+    for i in xrange(numbpixlpsfnside):
+        for j in xrange(numbpixlpsfnside):
+            # solve p = A coefspix for coefspix
+            p = psfnusampadd[i*factusam:(i+1)*factusam+1, j*factusam:(j+1)*factusam+1].flatten()
+            print 'A'
+            print A.shape
+            print 'p'
+            print p.shape
+            print 'coefspix[:, i, j]'
+            print coefspix[:, i, j].shape
+            print
+            coefspix[:, i, j] = np.dot(np.linalg.inv(np.dot(A.T, A)), np.dot(A.T, p)) 
+    coefspix = coefspix.reshape(coefspix.shape[0], coefspix.shape[1] * coefspix.shape[2])
+    
+    return coefspix
 
-def image_model_eval(x, y, f, back, imsz, nc, cf, lcpreval=None, regsize=None, margin=0, offsetx=0, offsety=0, weights=None, ref=None, lib=None):
+
+def image_model_eval(x, y, f, back, imsz, numbpixlpsfnside, coefspix, numbtime, booltimebins, lcpreval, \
+                     regsize=None, margin=0, offsetx=0, offsety=0, weig=None, ref=None, lib=None):
+    
     assert x.dtype == np.float32
     assert y.dtype == np.float32
     assert f.dtype == np.float32
-    assert cf.dtype == np.float32
+    assert coefspix.dtype == np.float32
     if ref is not None:
         assert ref.dtype == np.float32
+    
+    numbpixlpsfn = numbpixlpsfnside**2
 
-    if weights is None:
-        weights = np.full(imsz, 1., dtype=np.float32)
+    numbparaspix = coefspix.shape[0]
+
+    if weig is None:
+        weig = np.full(imsz, 1., dtype=np.float32)
     if regsize is None:
         regsize = max(imsz[0], imsz[1])
 
@@ -49,25 +70,25 @@ def image_model_eval(x, y, f, back, imsz, nc, cf, lcpreval=None, regsize=None, m
     y = y.compress(goodsrc)
     f = f.compress(goodsrc)
 
-    nstar = x.size
-    rad = nc/2 # 12 for nc = 25
+    numbphon = x.size
+    rad = numbpixlpsfnside / 2 # 12 for numbpixlpsfnside = 25
 
-    nregy = imsz[1]/regsize + 1 # assumes imsz % regsize = 0?
-    nregx = imsz[0]/regsize + 1
+    nregy = imsz[1] / regsize + 1 # assumes imsz % regsize = 0?
+    nregx = imsz[0] / regsize + 1
 
     ix = np.ceil(x).astype(np.int32)
     dx = ix - x
     iy = np.ceil(y).astype(np.int32)
     dy = iy - y
 
-    dd = np.column_stack((np.full(nstar, 1., dtype=np.float32), dx, dy, dx*dx, dx*dy, dy*dy, dx*dx*dx, dx*dx*dy, dx*dy*dy, dy*dy*dy)).astype(np.float32) * f[:, None]
+    dd = np.column_stack((np.full(numbphon, 1., dtype=np.float32), dx, dy, dx*dx, dx*dy, dy*dy, dx*dx*dx, dx*dx*dy, dx*dy*dy, dy*dy*dy)).astype(np.float32) * f[:, None]
 
     if lib is None:
         image = np.full((imsz[1]+2*rad+1,imsz[0]+2*rad+1), back, dtype=np.float32)
-        recon2 = np.dot(dd, cf).reshape((nstar,nc,nc))
-        recon = np.zeros((nstar,nc,nc), dtype=np.float32)
+        recon2 = np.dot(dd, coefspix).reshape((numbphon,numbpixlpsfnside,numbpixlpsfnside))
+        recon = np.zeros((numbphon,numbpixlpsfnside,numbpixlpsfnside), dtype=np.float32)
         recon[:,:,:] = recon2[:,:,:]
-        for i in xrange(nstar):
+        for i in xrange(numbphon):
             image[iy[i]:iy[i]+rad+rad+1,ix[i]:ix[i]+rad+rad+1] += recon[i,:,:]
 
         image = image[rad:imsz[1]+rad,rad:imsz[0]+rad]
@@ -82,23 +103,46 @@ def image_model_eval(x, y, f, back, imsz, nc, cf, lcpreval=None, regsize=None, m
                 x0 = max(j*regsize - offsetx - margin, 0)
                 x1 = min((j+1)*regsize - offsetx + margin, imsz[0])
                 subdiff = diff[y0:y1,x0:x1]
-                diff2[i,j] = np.sum(subdiff*subdiff*weights[y0:y1,x0:x1])
+                diff2[i,j] = np.sum(subdiff*subdiff*weig[y0:y1,x0:x1])
     else:
-        image = np.full((imsz[1], imsz[0]), back, dtype=np.float32)
-        recon = np.zeros((nstar,nc*nc), dtype=np.float32)
+        recon = np.zeros((numbphon, numbpixlpsfn), dtype=np.float32)
         reftemp = ref
         if ref is None:
-                reftemp = np.zeros((imsz[1], imsz[0]), dtype=np.float32)
-        diff2 = np.zeros((nregy, nregx), dtype=np.float64)
-        
-        if True:#False:
-            print 'weights'
-            print weights.shape
+            reftemp = np.zeros((imsz[1], imsz[0]), dtype=np.float32)
+        if booltimebins:
+            image = np.full((numbtime, imsz[1], imsz[0]), back, dtype=np.float32)
+            diff2 = np.zeros((numbtime, nregy, nregx), dtype=np.float64)
+        else:
+            image = np.full((imsz[1], imsz[0]), back, dtype=np.float32)
+            diff2 = np.zeros((nregy, nregx), dtype=np.float64)
+        if True:
+        #if False:
+            print 'dd'
+            summgene(dd)
+            print 'coefspix'
+            summgene(coefspix)
+            print 'recon'
+            summgene(recon)
+            print 'image'
+            summgene(image)
+            print 'weig'
+            print weig.shape
             print 'reftemp'
             print reftemp.shape
             print 'diff2'
             print diff2.shape
-        lib(imsz[0], imsz[1], nstar, nc, cf.shape[0], dd, cf, recon, ix, iy, image, reftemp, weights, diff2, regsize, margin, offsetx, offsety)
+            print 'lcpreval'
+            summgene(lcpreval)
+            print 'booltimebins'
+            print booltimebins
+            print type(booltimebins)
+            print 'numbtime'
+            print numbtime
+            print type(numbtime)
+            print
+
+        lib(imsz[0], imsz[1], numbphon, numbpixlpsfnside, numbparaspix, dd, coefspix, recon, ix, iy, image, \
+                        reftemp, weig, diff2, regsize, margin, offsetx, offsety, numbtime, booltimebins, lcpreval)
 
     if ref is not None:
         return image, diff2
