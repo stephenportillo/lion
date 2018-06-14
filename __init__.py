fb16668dee0ee2abf4ef574bd2628d4888099f2d
tdaylan <tansu.daylan@gmail.com>
2018-06-14 10:33:08 -0400

    Started putting common functions in __init__.py

--- __init__.py
+++ __init__.py
@@ -1,2 +1,14 @@
 # lets make this a package
+import numpy as np
+
+def summgene(varb):
+    
+    if varb == None:
+        print varb
+    else:
+        print np.amin(varb)
+        print np.amax(varb)
+        print np.mean(varb)
+        print varb.shape
+
 
